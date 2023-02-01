48f8ccafcb75d393842c8e57755a1da4ca49745c
Ritik Ranjan <rritik772@outlook.com>
2023-02-01 21:27:30 +0530

    day 8 part 1 new code

--- src/day_8.rs
+++ src/day_8.rs
@@ -1,5 +1,5 @@
-pub fn run(lines: Vec<String>) -> usize {
-    second(lines)
+pub fn run() -> usize {
+    main()
 }
 
 fn first(lines: Vec<String>) -> usize {
@@ -182,4 +182,121 @@ fn second(lines: Vec<String>) -> usize {
     }
 
     max
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct GridCoord {
+    x: usize,
+    y: usize,
+}
+
+impl std::fmt::Debug for GridCoord {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl From<(usize, usize)> for GridCoord {
+    fn from((x, y): (usize, usize)) -> Self {
+        Self { x, y }
+    }
+}
+
+struct Grid<T> {
+    width: usize,
+    height: usize,
+    data: Vec<T>,
+}
+
+impl<T> Grid<T>
+where
+    T: Default + Clone,
+{
+    fn new(width: usize, height: usize) -> Self {
+        Self {
+            width,
+            height,
+            data: vec![T::default(); width * height],
+        }
+    }
+
+    fn in_bounds(&self, coord: GridCoord) -> bool {
+        coord.x < self.width && coord.y < self.height
+    }
+
+    fn cell_mut(&mut self, coord: GridCoord) -> Option<&mut T> {
+        if !self.in_bounds(coord) {
+            return None;
+        }
+        Some(&mut self.data[coord.y * self.width + coord.x])
+    }
+
+    fn cell(&self, coord: GridCoord) -> Option<&T> {
+        if !self.in_bounds(coord) {
+            return None;
+        }
+        Some(&self.data[coord.y * self.width + coord.x])
+    }
+
+    fn width(&self) -> usize {
+        self.width
+    }
+
+    fn height(&self) -> usize {
+        self.height
+    }
+}
+
+fn parse_grid(input: &str) -> Grid<usize> {
+    let width = input.lines().next().unwrap().len();
+    let height = input.lines().count();
+
+    let mut grid = Grid::new(width, height);
+    for (y, line) in input.lines().enumerate() {
+        for (x, col) in line.chars().enumerate() {
+            assert!(col.is_ascii_digit());
+            *grid.cell_mut((x, y).into()).unwrap() = col as usize - '0' as usize;
+        }
+    }
+
+    grid
+}
+
+fn main() -> usize {
+    let grid = parse_grid(include_str!("./input"));
+    // println!("grid at (0, 0): {:?}", grid.cell((0, 0).into()));
+    // println!("grid at (3, 0): {:?}", grid.cell((3, 0).into()));
+    // println!("grid at (0, 2): {:?}", grid.cell((0, 2).into()));
+
+    part_1(grid)
+}
+
+fn part_1(grid: Grid<usize>) -> usize {
+    let all_coords = (0..grid.height()).into_iter().flat_map(|y| {
+        (0..grid.width())
+            .into_iter()
+            .map(move |x| GridCoord::from((x, y)))
+    });
+
+    let nums_visible_cells = all_coords
+        .filter(|&coord| {
+            let coord_height = grid.cell(coord).unwrap();
+            let deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+
+            deltas.iter().any(|&(dx, dy)| {
+                let mut cells_in_line = (1..).into_iter().map_while(|i| {
+                    let coord = GridCoord {
+                        x: coord.x.checked_add_signed(dx * i)?,
+                        y: coord.y.checked_add_signed(dy * i)?,
+                    };
+
+                    grid.cell(coord)
+                });
+
+                cells_in_line.all(|height| height < coord_height)
+            })
+        })
+        .count();
+
+    return nums_visible_cells;
 }
